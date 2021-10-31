dacef372f6588cd8f51545cb4b0c13b267cc29cc
Evel-Devel <zhenka_nikitin@mail.ru>
2021-10-31 17:19:04 +0700

    05-WordScramble - Setup project

---
+++ 05-WordScramble/05-WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  05-WordScramble
+//
+//  Created by Евгений Никитин on 31.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
