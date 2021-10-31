815b9c648b32f309e17578a9eface64a0ed7a593
Evel-Devel <zhenka_nikitin@mail.ru>
2021-10-31 18:51:29 +0700

    05-WordScramble - Tweak the game logic, add checks. Refactoring

--- 05-WordScramble/05-WordScramble/ViewController.swift
+++ 05-WordScramble/05-WordScramble/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UITableViewController {
 }
 
 
-// MARK: Private methods
+// MARK: Privates
 extension ViewController {
     
     private func setup() {
@@ -69,17 +69,68 @@ extension ViewController {
         ac.addAction(submitAction)
         present(ac, animated: true)
     }
+}
+
+
+// MARK: Game logic
+extension ViewController {
     
     private func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
         
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    private func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else {
+            return false
+        }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    private func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        let misspelledRange = checker.rangeOfMisspelledWord(
+            in: word,
+            range: range,
+            startingAt: 0,
+            wrap: false,
+            language: "en")
+        
+        return misspelledRange.location == NSNotFound
     }
 }
 
 
-// MARK: Table view delegates
+// MARK: Table delegates
 extension ViewController {
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         usedWords.count
     }
     
