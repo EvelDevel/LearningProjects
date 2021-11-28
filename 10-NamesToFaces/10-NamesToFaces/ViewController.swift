57605e2c66945461c17a527b5940798f83261bc5
Evel-Devel <zhenka_nikitin@mail.ru>
2021-11-28 16:37:52 +0700

    10-NamesToFaces - Add image picker with userDefaults

--- 10-NamesToFaces/10-NamesToFaces/ViewController.swift
+++ 10-NamesToFaces/10-NamesToFaces/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
     }
+    
+    private func setup() {
+        setupNavigationItems()
+    }
+    
+    private func setupNavigationItems() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewPerson))
+    }
+    
+    @objc private func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+}
+
+
+// MARK: Handle Collection
+extension ViewController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
@@ -25,6 +49,33 @@ class ViewController: UICollectionViewController {
             fatalError("Unable to dequeue PersonCell")
         }
         return cell
+    }
+}
+
+
+// MARK: Delegates
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            guard let image = info[.editedImage] as? UIImage else {
+                return
+            }
+            let imageName = UUID().uuidString
+            let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+            
+            if let jpegData = image.jpegData(compressionQuality: 0.8) {
+                try? jpegData.write(to: imagePath)
+            }
+            
+            dismiss(animated: true)
+        }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
 
