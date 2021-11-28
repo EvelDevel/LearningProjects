abbf6c8571402aa4e75169996e1cdecfc8cb0e1f
Evel-Devel <zhenka_nikitin@mail.ru>
2021-11-28 15:46:55 +0700

    10-NamesToFaces - Create project, add UI

---
+++ 10-NamesToFaces/10-NamesToFaces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  10-NamesToFaces
+//
+//  Created by Евгений Никитин on 28.11.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    
+    
+}
---
+++ 10-NamesToFaces/10-NamesToFaces/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  10-NamesToFaces
+//
+//  Created by Евгений Никитин on 28.11.2021.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
