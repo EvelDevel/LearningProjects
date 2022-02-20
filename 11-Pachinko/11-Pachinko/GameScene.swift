435a274a8109efa063b8ea3db0fd98e3eb6f7561
Evel-Devel <zhenka_nikitin@mail.ru>
2022-02-20 11:33:42 +0700

    11 - Pachinko - Add balls, add bouncers

--- 11-Pachinko/11-Pachinko/GameScene.swift
+++ 11-Pachinko/11-Pachinko/GameScene.swift
@@ -10,24 +10,25 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        setupBackground()
+        addBackground()
+        setupBouncers()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {
             return
         }
-        
         let location = touch.location(in: self)
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-        box.position = location
-        addChild(box)
+        
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
     }
     
     /// Privates
-    private func setupBackground() {
-        
+    private func addBackground() {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
@@ -35,5 +36,21 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    private func setupBouncers() {
+        addBouncer(at: CGPoint(x: 0, y: 0))
+        addBouncer(at: CGPoint(x: 256, y: 0))
+        addBouncer(at: CGPoint(x: 512, y: 0))
+        addBouncer(at: CGPoint(x: 768, y: 0))
+        addBouncer(at: CGPoint(x: 1024, y: 0))
+    }
+    
+    private func addBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
     }
 }
