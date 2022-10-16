ecae52f872a38553499f932868efd4c0627c3229
Evel-Devel <zhenka_nikitin@mail.ru>
2022-10-16 18:56:51 +0700

    Add more logic

---
+++ Landmarks/Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Евгений Никитин on 16.10.2022.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 120, height: 120)
+                .cornerRadius(8)
+            Text(landmark.name)
+                .foregroundColor(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
