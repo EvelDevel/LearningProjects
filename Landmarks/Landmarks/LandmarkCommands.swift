90e183c521a5a07c4d6a20a8b24a5dbb4f638a67
Evel-Devel <zhenka_nikitin@mail.ru>
2023-01-18 12:03:00 +0700

    Start working on Mac target

---
+++ Landmarks/Landmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Евгений Никитин on 18.01.2023.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -10,11 +10,17 @@ import SwiftUI
 @main
 struct LandmarksApp: App {
     @StateObject private var modelData = ModelData()
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(modelData)
         }
+        
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
     }
 }
