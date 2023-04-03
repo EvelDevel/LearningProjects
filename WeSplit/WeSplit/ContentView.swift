0cb47529caaddf4fdee8a497c8303cbb9287b66a
Evel-Devel <zhenka_nikitin@mail.ru>
2023-04-03 10:39:07 +0700

    Add some modifications before remove all and start real project

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,11 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tapCount = 0
+    @State private var name = ""
+    @State private var selectedStudent = "Harry"
+    
+    let students = ["Harry", "Hermione", "Ron"]
     
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
         }
+
+        // Form {
+        //     Group {
+        //         Button("Tap Count: \(tapCount)") {
+        //             tapCount += 1
+        //         }
+        //
+        //         Section {
+        //             TextField("Enter your name", text: $name)
+        //             Text("Your name is \(name)")
+        //         }
+        //     }
+        // }
     }
 }
 
