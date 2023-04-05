3585a8e10ed993df5a09801b14ab91632fa42c71
Evel-Devel <zhenka_nikitin@mail.ru>
2023-04-05 12:16:47 +0700

    WeSplit - Trying some color staff

---
+++ WeSplit/WeSplit/ColorPractice.swift
@@ -0,0 +1,32 @@
+//
+//  ColorPractice.swift
+//  WeSplit
+//
+//  Created by Евгений Никитин on 05.04.2023.
+//
+
+import SwiftUI
+
+struct ColorPractice: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Color.indigo
+                Color.mint
+            }
+            .ignoresSafeArea()
+            
+            Text("Hello")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .cornerRadius(50)
+                .background(.ultraThinMaterial)
+        }
+    }
+}
+
+struct ColorPractice_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPractice()
+    }
+}
