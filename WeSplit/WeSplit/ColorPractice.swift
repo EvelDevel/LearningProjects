074bd2fe510f504c2d7a398e5de0db2931e88ff9
Evel-Devel <zhenka_nikitin@mail.ru>
2023-04-17 09:43:52 +0700

    WeSplit - practice with buttons and styles

--- WeSplit/WeSplit/ColorPractice.swift
+++ WeSplit/WeSplit/ColorPractice.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct ColorPractice: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.indigo
-                Color.mint
+        Group {
+            ZStack {
+                VStack(spacing: 0) {
+                    Color.indigo
+                    Color.mint
+                }
+                .ignoresSafeArea()
+                
+                Text("Hello")
+                    .foregroundStyle(.secondary)
+                    .padding(50)
+                    .cornerRadius(50  )
+                    .background(.ultraThinMaterial)
             }
-            .ignoresSafeArea()
             
-            Text("Hello")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .cornerRadius(50)
-                .background(.ultraThinMaterial)
+            VStack {
+                Button("Button 1") {}
+                    .buttonStyle(.bordered)
+                
+                Button("Button 2", role: .destructive) {}
+                    .buttonStyle(.bordered)
+                
+                Button("Button 3") {}
+                    .buttonStyle(.borderedProminent)
+                    .tint(.mint)
+                
+                Button("Button 4", role: .destructive) {}
+                    .buttonStyle(.borderedProminent)
+            }
         }
     }
 }
