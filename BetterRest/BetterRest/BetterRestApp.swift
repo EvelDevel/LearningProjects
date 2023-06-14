6c8f0dc1abbdce484fc320b8c7f18852876f2457
Evgeny Nikitin <enikitin@icerockdev.com>
2023-06-14 11:12:44 +0700

    BetterRest - Start new project with Create ML

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Евгений Никитин on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -29,37 +29,101 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
-                .ignoresSafeArea()
+            // LinearGradient(
+            //     gradient: Gradient(
+            //         colors: [.indigo, .black]
+            //     ),
+            //     startPoint: .top,
+            //     endPoint: .bottom
+            // )
+            //     .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag off")
-                    Text(countries[correctAnswer])
-                }
-                .foregroundColor(.white)
+            RadialGradient(
+                stops: [
+                    .init(
+                        color: Color(
+                            red: 0.1,
+                            green: 0.2,
+                            blue: 0.45
+                        ),
+                        location: 0.3
+                    ),
+                    .init(
+                        color: Color(
+                            red: 0.76,
+                            green: 0.15,
+                            blue: 0.26
+                        ),
+                        location: 0.3
+                    )
+                ],
+                center: .top,
+                startRadius: 200,
+                endRadius: 700
+            )
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundColor(.white)
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                            .foregroundColor(.white)
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(RoundedRectangle(
+                                    cornerRadius: 8,
+                                    style: .continuous
+                                ))
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(minWidth: 270)
+                .padding(.vertical, 35)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+            
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
         }
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your score is \(score)")
-        }
+        
+        // .alert(scoreTitle, isPresented: $showingScore) {
+        //     Button("Continue", action: askQuestion)
+        // } message: {
+        //     Text("Your score is \(score)")
+        // }
     }
     
     private func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            askQuestion()
         } else {
             scoreTitle = "Wrong"
         }
