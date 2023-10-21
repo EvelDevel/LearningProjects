495eb0af34ddd5e2f98fe4ead1aa55bba9d8073b
Evgeny Nikitin <zhenka_nikitin@mail.ru>
2023-10-21 17:11:37 +0700

    Add texts examples

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/TextBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  TextBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Евгений Никитин on 21.10.2023.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Hello, World!")
+                .font(.body)
+                .bold()
+                .underline(true, color: .accentColor)
+            
+            Text("System text for example")
+                .font(
+                    .system(
+                        size: 16,
+                        weight: .heavy,
+                        design: .serif
+                    )
+                )
+        }
+    }
+}
+
+struct TextBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBootcamp()
+    }
+}
